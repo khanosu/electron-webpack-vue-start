e491acd5270aed56112aebedc26b5ca64115136a
Furrukh Khan <khan.1@osu.edu>
2018-12-31 15:55:49 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# electron-webpack-vue-start
+
+Built from scratch, Webpack based Electron & Vue app with Babel
+
+Uses Webpack 4.x
+
+Uses Babel
+
+Single page Vue components are supported
+ 
---
+++ components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>{{name}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "Columbus!"
+    };
+  }
+};
+</script>
+
+<style scoped>
+h1 {
+  color: green;
+}
+</style>
